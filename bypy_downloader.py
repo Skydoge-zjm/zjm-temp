3f76b7ae52b66cb6f3766055d6fdc7451824c2ee
Skydoge-zjm <2984095138@qq.com>
2025-02-27 12:47:58 +0800

    1

---
+++ bypy_downloader.py
@@ -0,0 +1,18 @@
+from bypy import ByPy
+import sys
+
+
+bp = ByPy(
+    configdir="./bypy_config",
+    downloader="aria2",
+    retry=10,
+)
+print("=============== files under /app/ ===============")
+bp.list()
+print("=============== start downloading ===============")
+
+"""
+bp.download(
+    remotepath=sys.argv[1],
+    localpath=sys.argv[2],
+)"""
